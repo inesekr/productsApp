3dcf4b6d2c7be00bb6da3d256475783a686d0d74
Inese Krole <inese.krole@gmail.com>
2023-11-14 13:11:08 +0200

    added search fields

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import {FC} from 'react';
 import  {BrowserRouter, Routes, Route}  from 'react-router-dom';
 import ProductList from './components/ProductList';
 import ProductDetailsWrapper from './components/ProductDetailsWrapper';
+import ProductDetails from './components/ProductDetails';
+import { ProductProvider } from './components/ProductContext';
 
 import './App.css';
 
 const App: FC = () => {
 
   return (
- 
+    <>
+    <h1>Products App</h1>
+    <ProductProvider>
     <BrowserRouter>
       <Routes>
         <Route
@@ -19,7 +23,8 @@ const App: FC = () => {
        
       </Routes>
     </BrowserRouter>
-    
+    </ProductProvider>
+    </>
    );
 };
 
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,45 +1,68 @@
-import { FC, useEffect, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import axios from 'axios';
 import Product from './Product';
 import { Product as ProductType } from './ProductTypes';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { useProductContext } from './ProductContext';
 import '../styles/Product.css';
 
-const ProductList:FC = () => {
-    const [products, setProducts] = useState<ProductType[]>([]);
-    const navigate = useNavigate();
-  
-    useEffect(() => {
-      const fetchProducts = async () => {
-        try {
-          const response = await axios.get('https://run.mocky.io/v3/b54fe93f-f5a1-426b-a76c-e43d246901fd');
-          const productsArray = response.data.products || []; 
-          setProducts(productsArray);            
-        } catch (error) {
-          console.error('Error fetching products:', error);
-        }
-      };
-  
-      fetchProducts();
-    }, []);
-  
-    const handleProductClick = (product: ProductType) => {
-      // Navigate to ProductDetails with the product ID included in the URL
-      navigate(`/product/${product.id}`, { state: { product } });
-    };
-    
-    return (
-      <div className="product-list-container">
-        {products.map((product) => (
-        <div key={product.id} className="product-link" onClick={() => handleProductClick(product)}>
-          <Product product={product} />
-        </div>
-      ))}
-         
-      </div>
-    );
-  
+const ProductList: FC = () => {
+  const { products: allProducts } = useProductContext();
+  const [nameSearchTerm, setNameSearchTerm] = useState('');
+  const [categorySearchTerm, setCategorySearchTerm] = useState('');
+  const [filteredProducts, setFilteredProducts] = useState<ProductType[]>(allProducts);
+
+  useEffect(() => {
+    const delayedSearch = setTimeout(() => {
+      // Filter products based on the search term
+      const filtered = allProducts.filter((product) =>
+        product.name.toLowerCase().includes(nameSearchTerm.toLowerCase()) &&  product.category.toLowerCase().includes(categorySearchTerm.toLowerCase())
+      );
+
+      setFilteredProducts(filtered);
+    }, 300); // Adjust the delay time as needed
+
+    return () => clearTimeout(delayedSearch); // Cleanup the timeout on component unmount or when the search term changes
+  }, [nameSearchTerm, categorySearchTerm, allProducts]);
+
+  const handleNameSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNameSearchTerm(event.target.value);
   };
-  
-  export default ProductList;
-  
+
+  const handleCategorySearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCategorySearchTerm(event.target.value);
+  };
+
+  return (
+    <div>
+      <div className="input-container">
+      <input className="input-field"
+        type="text"
+        placeholder="Search by name..."
+        value={nameSearchTerm}
+        onChange={handleNameSearchChange}
+      />
+      <div className="or-separator">
+        <p>or</p>
+      </div>
+      <input className="input-field"
+        type="text"
+        placeholder="Search by category..."
+        value={categorySearchTerm}
+        onChange={handleCategorySearchChange}
+      />
+      </div>
+      
+
+      <div className="product-list-container">
+        {filteredProducts.map((product) => (
+          <Link key={product.id} to={`/product/${product.id}`} className="product-link">
+            <Product product={product} />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
